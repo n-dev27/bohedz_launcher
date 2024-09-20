69e08f83a160963cc751c62dd9e668497b5909d4
CoCo-27 <cocokingdom27@gmail.com>
2024-09-20 17:02:57 -0400

    ☑☑ update video section

--- components/Common/SectionTitle.tsx
+++ components/Common/SectionTitle.tsx
@@ -16,12 +16,12 @@ const SectionTitle = ({
       <div
         className={`wow fadeInUp w-full ${center ? "mx-auto text-center" : ""}`}
         data-wow-delay=".1s"
-        style={{ maxWidth: width, marginBottom: mb }}
+        style={{ maxWidth: '920px', marginBottom: mb }}
       >
-        <h2 className="mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]">
+        <h2 className="mb-4 text-3xl font-bold !leading-tight text-black sm:text-4xl md:text-5xl">
           {title}
         </h2>
-        <p className="text-base !leading-relaxed text-body-color md:text-lg">
+        <p className="text-base !leading-relaxed text-[rgba(51,65,85,1)] md:text-lg px-8 md:px-20">
           {paragraph}
         </p>
       </div>
--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -4,61 +4,105 @@ import Image from "next/image";
 import { useState } from "react";
 import SectionTitle from "../Common/SectionTitle";
 
-import ModalVideo from "react-modal-video";
-
 const Video = () => {
-  const [isOpen, setOpen] = useState(false);
 
   return (
-    <section className="relative z-10 py-16 md:py-20 lg:py-28">
-      <div className="container">
+    <section className="relative z-10 py-12 md:py-16 lg:py-20">
+      <div className="container flex flex-col gap-20 justify-center items-center">
         <SectionTitle
-          title="We are ready to help"
-          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+          title="No compromises, no matter the choice."
+          paragraph="We offer a number of options, so we include a standard set of features with all our tokens. All of these features are included with all of our tokens, for no extra cost, so you can rest assured that you already have the best."
           center
-          mb="80px"
+          mb="20px"
         />
 
-        <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4">
-            <div
-              className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
-              data-wow-delay=".15s"
-            >
-              <div className="relative aspect-[77/40] items-center justify-center">
-                <Image src="/images/video/video.jpg" alt="video image" fill />
-                <div className="absolute top-0 right-0 flex h-full w-full items-center justify-center">
-                  <button
-                    onClick={() => setOpen(true)}
-                    className="flex h-[70px] w-[70px] items-center justify-center rounded-full bg-white bg-opacity-75 text-primary transition hover:bg-opacity-100"
-                  >
-                    <svg
-                      width="16"
-                      height="18"
-                      viewBox="0 0 16 18"
-                      className="fill-current"
-                    >
-                      <path d="M15.5 8.13397C16.1667 8.51888 16.1667 9.48112 15.5 9.86602L2 17.6603C1.33333 18.0452 0.499999 17.564 0.499999 16.7942L0.5 1.20577C0.5 0.43597 1.33333 -0.0451549 2 0.339745L15.5 8.13397Z" />
-                    </svg>
-                  </button>
-                </div>
-              </div>
+        <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
+          {/* Row 1 */}
+          <div className="flex flex-col justify-between items-center gap-4">
+            <Image
+              src="/images/video/compliant.svg"
+              alt="compliant"
+              width={64}
+              height={64}
+            />
+            <div className="flex flex-col items-center justify-center gap-2">
+              <p className="text-[rgba(51,65,85,1)] text-sm text-center font-medium">Spec Compliant</p>
+              <p className="text-[rgba(15,23,42,1)] text-xl text-center font-normal">Works with every wallet and exchange.</p>
+              <p className="text-[rgba(100,116,139,1)] text-sm text-center font-normal">All of our tokens go through rigorous testing to make sure they are fully compliant with the standards.</p>
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-between items-center gap-4">
+            <Image
+              src="/images/video/audited.svg"
+              alt="audited"
+              width={64}
+              height={64}
+            />
+            <div className="flex flex-col items-center justify-center gap-2">
+              <p className="text-[rgba(51,65,85,1)] text-sm text-center font-medium">Audited</p>
+              <p className="text-[rgba(15,23,42,1)] text-xl text-center font-normal">Tested by the best.</p>
+              <p className="text-[rgba(100,116,139,1)] text-sm text-center font-normal">All of our tokens have been audited to ensure the best security practices and standards are taken care of.</p>
+            </div>
+          </div>
+          
+          <div className="flex flex-col justify-between items-center gap-4">
+            <Image
+              src="/images/video/formally.svg"
+              alt="formally"
+              width={64}
+              height={64}
+            />
+            <div className="flex flex-col items-center justify-center gap-2">
+              <p className="text-[rgba(51,65,85,1)] text-sm text-center font-medium">Formally Verified</p>
+              <p className="text-[rgba(15,23,42,1)] text-xl text-center font-normal">The highest security standard.</p>
+              <p className="text-[rgba(100,116,139,1)] text-sm text-center font-normal">All our tokens go through a formal verification process to ensure they are safe and secure.</p>
+            </div>
+          </div>
+          {/* Row 2 */}
+          <div className="flex flex-col justify-between items-center gap-4">
+            <Image
+              src="/images/video/source.svg"
+              alt="source"
+              width={64}
+              height={64}
+            />
+            <div className="flex flex-col items-center justify-center gap-2">
+              <p className="text-[rgba(51,65,85,1)] text-sm text-center font-medium">Source Code Verified</p>
+              <p className="text-[rgba(15,23,42,1)] text-xl text-center font-normal">Verified before you're done.</p>
+              <p className="text-[rgba(100,116,139,1)] text-sm text-center font-normal">All of our tokens are pre-verified on all major block explorers, they show up as verified immediately.</p>
+            </div>
+          </div>
+          
+          <div className="flex flex-col justify-between items-center gap-4">
+            <Image
+              src="/images/video/access.svg"
+              alt="access"
+              width={64}
+              height={64}
+            />
+            <div className="flex flex-col items-center justify-center gap-2">
+              <p className="text-[rgba(51,65,85,1)] text-sm text-center font-medium">Advanced Access Control</p>
+              <p className="text-[rgba(15,23,42,1)] text-xl text-center font-normal">You're the only one with access.</p>
+              <p className="text-[rgba(100,116,139,1)] text-sm text-center font-normal">Our tokens come with advance access control, making sure that only you have access to all token functions.</p>
+            </div>
+          </div>
+          
+          <div className="flex flex-col justify-between items-center gap-4">
+            <Image
+              src="/images/video/trust.svg"
+              alt="trust"
+              width={64}
+              height={64}
+            />
+            <div className="flex flex-col items-center justify-center gap-2">
+              <p className="text-[rgba(51,65,85,1)] text-sm text-center font-medium">Trust & Confidence</p>
+              <p className="text-[rgba(15,23,42,1)] text-xl text-center font-normal">Immaculate track record.</p>
+              <p className="text-[rgba(100,116,139,1)] text-sm text-center font-normal">We've been building tokens for 5+ years, our tokens are used by thousands of people. Unbeatable track record.</p>
             </div>
           </div>
         </div>
-      </div>
 
-      <ModalVideo
-        channel="youtube"
-        autoplay={true}
-        start={true}
-        isOpen={isOpen}
-        videoId="L61p2uyiMSo"
-        onClose={() => setOpen(false)}
-      />
-
-      <div className="absolute bottom-0 left-0 right-0 z-[-1]">
-        <img src="/images/video/shape.svg" alt="shape" className="w-full" />
       </div>
     </section>
   );
