44605bc0bb40bbfb0779db9dc18347d9d78a7c1e
CoCo-27 <cocokingdom27@gmail.com>
2024-09-20 20:00:46 -0400

    ☑☑ remove unnecessary

--- components/Common/SectionTitle.tsx
+++ components/Common/SectionTitle.tsx
@@ -14,14 +14,14 @@ const SectionTitle = ({
   return (
     <>
       <div
-        className={`wow fadeInUp w-full ${center ? "mx-auto text-center" : ""}`}
+        className={`wow fadeInUp w-full text-center ${center ? "mx-auto text-center" : ""}`}
         data-wow-delay=".1s"
-        style={{ maxWidth: width, marginBottom: mb }}
+        style={{ marginBottom: mb }}
       >
         <h2 className="mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]">
           {title}
         </h2>
-        <p className="text-base !leading-relaxed text-body-color md:text-lg">
+        <p className="max-w-[570px] mx-auto text-center text-base !leading-relaxed text-body-color md:text-lg">
           {paragraph}
         </p>
       </div>
